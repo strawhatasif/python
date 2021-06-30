2bc8838d1490a7b5de7892e5d3131af0c5e5d14b
Asif A Fasih <aafasih@gmail.com>
2021-06-29 19:24:49 -0500

    a program showcasing two simple for loops

---
+++ simpleLoop.py
@@ -0,0 +1,14 @@
+#  Two simple loops showing how to iterate.
+
+#  Iterate through each name in a list of names and print them out to the console.
+names = ['Rachel Belle Pepperstaffere', 'Laura Canbury', 'Samuel Riplee']
+
+for name in names:
+    print(name)
+
+#  Iterate through each number in a list of numbers and print them out to the console.
+
+numbers = [0, 100, 2560, 5520, 10250]
+
+for num in numbers:
+    print(num)
