69d8cd2a912abfeaae3575d01327d271747c5ec9
Asif A Fasih <aafasih@gmail.com>
2021-07-17 14:33:24 -0500

    refactoring test to use TestCase from unittest

--- test_greeting.py
+++ test_greeting.py
@@ -1,4 +1,4 @@
-import pytest
+from unittest import TestCase
 import greeting
 
 greeting = greeting.Greeting("Rachel Belle Pepperstaffere")
@@ -6,11 +6,12 @@ over_twenty_one_greeting = 'Welcome! It is our pleasure to treat you to an adult
 under_twenty_one_greeting = 'Hey there! Would you like coke or apple juice, Rachel Belle Pepperstaffere?'
 
 
-def test_greet_customer_younger_than_twenty_one():
-    customer_greeting = greeting.greet_customer(18)
-    assert customer_greeting == under_twenty_one_greeting
+class TestGreeting(TestCase):
 
+    def test_greet_customer_younger_than_twenty_one(self):
+        customer_greeting = greeting.greet_customer(18)
+        self.assertEqual(under_twenty_one_greeting, customer_greeting)
 
-def test_greet_customer_older_than_twenty_one():
-    customer_greeting = greeting.greet_customer(30)
-    assert customer_greeting == over_twenty_one_greeting
+    def test_greet_customer_older_than_twenty_one(self):
+        customer_greeting = greeting.greet_customer(30)
+        self.assertEqual(over_twenty_one_greeting, customer_greeting)
